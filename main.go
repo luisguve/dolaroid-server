2230d95065e60cd81bb51cdfb1914f198e311ca0
Luis G. Villegas <luisguve@gmail.com>
2020-11-14 14:35:41 -0430

    Enable TLS in production, set config parameters in config.toml and set cleanup interval to 30 minutes.

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"crypto/tls"
+	"fmt"
 	"log"
+	"net"
 	"time"
+	"strings"
 
 	"github.com/luisguve/scs/v2"
+	"github.com/BurntSushi/toml"
 	"github.com/alexedwards/scs/boltstore"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/luisguve/dolaroid-server/datastore"
 	"github.com/luisguve/dolaroid-server/sessionstore"
+
+	"golang.org/x/crypto/acme/autocert"
 )
+
+type srvConfig struct {
+	AllowOrigins []string `toml:"allow_origins"`
+	Production   bool     `toml:"production"`
+	Domain       string   `toml:"domain"`
+	Email        string   `toml:"email"`
+	SessionLT    int      `toml:"session_lifetime"`
+}
+
+func (s srvConfig) validate() error {
+	if len(s.AllowOrigins) == 0 {
+		return fmt.Errorf("empty origins list")
+	}
+	return nil
+}
 
 func main() {
 	data, err := datastore.New()
@@ -25,23 +47,48 @@ func main() {
 	}
 	defer sessStore.Close()
 
+	var config srvConfig
+	if _, err = toml.DecodeFile("config.toml", &config); err != nil {
+		log.Fatal(err)
+	}
+	if err = config.validate(); err != nil {
+		log.Fatal(err)
+	}
+
 	session := scs.New()
-	session.Store = boltstore.NewWithCleanupInterval(sessStore, 3 * time.Minute)
-	session.Lifetime = 24 * time.Hour
+	session.Store = boltstore.NewWithCleanupInterval(sessStore, 30 * time.Minute)
+	session.Lifetime = time.Duration(config.SessionLT) * time.Hour
 	r := routes{
 		ds: data,
 		sess: session,
 	}
 
 	app := fiber.New()
+	if config.Production {
+		if config.Domain == "" {
+			log.Fatal("Empty domain name in production")
+		}
+		certManager := autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: autocert.HostWhitelist(config.Domain),
+			Cache:      autocert.DirCache("certs"),
+			Email:      config.Email,
+		}
+
+		tlsConfig := certManager.TLSConfig()
+		ln, _ := net.Listen("tcp", ":443")
+		ln = tls.NewListener(ln, tlsConfig)
+		app.Listener(ln)
+	}
 
 	// Wrap handlers with session middleware.
 	app.Use("/", func(c *fiber.Ctx) error {
-		return r.LoadAndSave(c)
-	}, cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5000",
-		AllowCredentials: true,
-	}))
+			return r.LoadAndSave(c)
+		}, cors.New(cors.Config{
+			AllowOrigins: strings.Join(config.AllowOrigins, ","),
+			AllowCredentials: true,
+		}),
+	)
 
 	app.Get("/", r.handleIndex)
 
@@ -58,6 +105,6 @@ func main() {
 		return c.SendStatus(404) // => 404 "Not Found"
 	})
 
-	// Start server on http://localhost:80
-	app.Listen(":80")
+	// Start server on port 80, or port 443 if the environment is production.
+	log.Fatal(app.Listen("80"))
 }
